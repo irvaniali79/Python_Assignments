dfdfd1db62a3aa6bca771513763a903726ebc488
okabe <irvaniali79@gmail.com>
2021-12-16 00:07:08 +0330

    fxing agent

--- Assignment_11/snake/objects/Agent.py
+++ Assignment_11/snake/objects/Agent.py
@@ -7,27 +7,29 @@ import math
 class Agent(Snake):
     def __init__(self):
         super().__init__()
+        self.nearX=0
+        self.nearY=0
     
     
     def run(self,game):
+
         self.get_env(env=game)
         self.set_state(env=game)
 
     def get_env(self,env):
     
-        if math.sqrt((env.snake.center_x-env.apple.center_x)**2+(env.snake.center_y-env.apple.center_y)**2)<\
-            math.sqrt((env.snake.center_x-env.pear.center_x)**2+(env.snake.center_y-env.pear.center_y)**2):
-            self.nearX=env.apple.center_x
-            self.nearY=env.apple.center_y
+        if math.sqrt((self.center_x-env.apple.apple.center_x)**2+(self.center_y-env.apple.apple.center_y)**2)<\
+            math.sqrt((self.center_x-env.pear.pear.center_x)**2+(self.center_y-env.pear.pear.center_y)**2):
+            self.nearX=env.apple.apple.center_x
+            self.nearY=env.apple.apple.center_y
         else :
-            self.nearX=env.pear.center_x   
-            self.nearY=env.pear.center_y
+            self.nearX=env.pear.pear.center_x   
+            self.nearY=env.pear.pear.center_y
 
     def set_state(self,env):
-
+        
         if  self.center_x>self.nearX:
             env.on_key_release(symbol=arcade.key.LEFT,modifiers=None)
-
         elif self.center_x <self.nearX:
             env.on_key_release(symbol=arcade.key.RIGHT,modifiers=None)
 
