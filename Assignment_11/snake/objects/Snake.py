6a2100362ef024d694cdcb54a42769c822afc0dc
okabe <irvaniali79@gmail.com>
2021-12-15 23:09:47 +0330

    define run dummy method for structure

---
+++ Assignment_11/snake/objects/Snake.py
@@ -0,0 +1,71 @@
+import arcade
+from objects.Spirtes import *
+
+class Snake(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.width=30
+        self.height=30
+        self.center_x  = SCREEN_WIDTH //2
+        self.center_y  = SCREEN_HEIGHT //2
+        self.score = 0
+        self.body=[[SCREEN_WIDTH //2,SCREEN_HEIGHT //2,30,30,arcade.color.GREEN,None,None]]
+    def run(self,game):
+        pass
+    def move(self):
+
+        self.body[-1][5]=    self.body[0][0]
+        self.body[-1][6]=    self.body[0][1]
+        self.body[-1][0]=   self.body[0][0]
+        self.body[-1][1]=   self.body[0][1]
+        tmp=self.body[-1]
+        self.body.pop(-1)
+        self.body.insert(0,tmp)
+    def eat (self , obj_type):
+        
+            if obj_type == 'apple':      
+                self.score = self.score + 1
+                self.body.append([ self.body[-1][5],self.body[-1][6],30,30,arcade.color.GREEN,None,None])
+                return True
+
+            elif obj_type == 'pear':
+                self.score = self.score + 2
+                self.body.append([ self.body[-1][5],self.body[-1][6],30,30,arcade.color.GREEN,None,None])
+
+                if(self.body[-1][0]==self.body[-2][0]):
+
+                    if(self.body[-1][1]>self.body[-2][1]):
+                        self.body.append([ self.body[-1][0],self.body[-1][1]+20,30,30,arcade.color.GREEN,None,None])
+                        self.body[-1][5]=self.body[-1][0]
+                        self.body[-1][6]=self.body[-1][1]+20
+                        
+                    else:
+                        self.body.append([ self.body[-1][0],self.body[-1][1]-20,30,30,arcade.color.GREEN,None,None])
+                        self.body[-1][5]=self.body[-1][0]
+                        self.body[-1][6]=self.body[-1][1]-20
+
+                elif(self.body[-1][1]==self.body[-2][1]):
+
+                    if(self.body[-1][0]>self.body[-2][0]):
+                        self.body.append([ self.body[-1][0]+20,self.body[-1][1],30,30,arcade.color.GREEN,None,None])
+                        self.body[-1][5]=self.body[-1][0]+20
+                        self.body[-1][6]=self.body[-1][1]
+
+                    else:
+                        self.body.append([ self.body[-1][0]-20,self.body[-1][1],30,30,arcade.color.GREEN,None,None])
+                        self.body[-1][5]=self.body[-1][0]-20
+                        self.body[-1][6]=self.body[-1][1]
+
+                return True 
+            elif obj_type == 'shit':
+                if(self.score<0):
+                    return False
+
+                self.score = self.score - 1  
+                self.body.pop(-1)
+                return True       
+
+    def draw (self):
+        for knuckle in self.body:
+            arcade.draw_rectangle_filled(knuckle[0],knuckle[1],knuckle[2],knuckle[3],knuckle[4])
+              
