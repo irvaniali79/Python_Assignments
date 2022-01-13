3b25b63319905d9ca44f306197a8f0e24758b325
okabe-rintaru <irvaniali79@gmail.com>
2022-01-13 12:59:53 +0330

    Assignment_15

---
+++ Assignment_15/INSERT.sql
@@ -0,0 +1,4 @@
+INSERT INTO Products (`price`,`count`) VALUES (1000,3);
+INSERT INTO Products (`price`,`count`) VALUES (1300,1);
+INSERT INTO Products (`price`,`count`) VALUES (1400,1);
+INSERT INTO Products (`price`,`count`) VALUES (800,10);
